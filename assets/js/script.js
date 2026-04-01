ca293e6ea65cffd04fe2eb241a009406c80756a1
Mithesha Nuwanjana <nmithesha@gmail.com>
2026-04-01 15:32:04 +0530

    Create script.js for OLMS global JavaScript
    Add global JavaScript file for custom frontend interactivity.

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/* ==========================================================================
+   OLMS Global JavaScript
+   
+   Note: Put all custom frontend interactivity here.
+   Make sure to include this file at the bottom of includes/footer.php!
+   ========================================================================== */
